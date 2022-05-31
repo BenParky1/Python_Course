b69c8abf52dde4b89231027bac306a3efd18be67
BenParky1 <102144782+BenParky1@users.noreply.github.com>
2022-05-31 15:03:16 +0100

    First code module 1.2.5

---
+++ my_first_code.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+
+print("My name is Ben Parkinson")
+print("The date is Tuesday 31 May")
+
+
+# In[5]:
+
+
+# Write a program to analyse real estate data - Calculate Total sales
+
+# Variables - house sale values
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# x = Total Sales
+x = print(a + b + c + d + e)
+
+
+# In[7]:
+
+
+#Av. sales for month, y = number of sales
+x = 182900
+y = 5
+
+print(x/y)
+
+
+# In[9]:
+
+
+# Missing Property price - Sale price + commission
+
+print(c + 6500)
+
+
+# In[ ]:
+
+
+
+
